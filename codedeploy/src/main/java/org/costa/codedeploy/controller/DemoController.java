90a8e5381a273eb6076c16202d6203b9e0a55495
Krishantha Costa <mkncosta@gmail.com>
2018-10-22 16:59:48 +0530

    First commit done.

---
+++ codedeploy/src/main/java/org/costa/codedeploy/controller/DemoController.java
@@ -0,0 +1,16 @@
+package org.costa.codedeploy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DemoController {
+	
+	@RequestMapping(value= {"/", "/home", "/index"})
+	public ModelAndView homeRequest() {
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("status","This is done by me");
+		return mv;
+	}
+}
